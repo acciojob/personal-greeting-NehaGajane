ca8a6800f974e297be92fabd956b87f62c95ee70
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-03-05 20:22:55 +0530

    Modified solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,20 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text, setText] = useState('');
+
+  const inputChange = (event) => {
+    setText(event.target.value);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
       <label>Enter your name:</label> <br></br>
       <input onChange={inputChange} type='text'></input>
-      <p>{text}</p>
+      {text && 
+        <p>{text}</p>
+      }
     </div>
   )
 }
