d5954179b0e02a7ceb8c588582e4ad60f4667ea0
NehaGajane <137098317+NehaGajane@users.noreply.github.com>
2024-03-05 20:19:12 +0530

    Added Solution

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <label>Enter your name:</label> <br></br>
+      <input onChange={inputChange} type='text'></input>
+      <p>{text}</p>
     </div>
   )
 }
